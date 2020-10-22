735a57750aa4feadb405310bd7ca51f4447a57c5
Allison Barnard <61567721+allisonbarnard07@users.noreply.github.com>
2020-10-21 22:13:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Write password to the #password input
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
 }
 
-//Code to generate password with options for user to choose from if/while
 
 function generatePassword() {
     var result = "";
